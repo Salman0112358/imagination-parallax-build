9c243c628bb4c7b6f0b5e47169edcf49a3f4440d
Salman0112358 <Salman0112358@outlook.com>
2022-10-31 23:48:34 +0000

    importing supabase set up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,21 @@ import "../styles/globals.css";
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from "@nextui-org/react";
 
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { use, useState } from "react";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </SessionContextProvider>
   );
 }
