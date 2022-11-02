70e7167ab06e2214b7fb30c50a79dec546a7a683
Salman0112358 <Salman0112358@outlook.com>
2022-11-02 21:56:30 +0000

    added state tracking for the prompt inputs

--- pages/promptbuilder/index.tsx
+++ pages/promptbuilder/index.tsx
@@ -1,7 +1,14 @@
+import { useUser } from "@supabase/auth-helpers-react";
 import Head from "next/head";
-import React from "react";
+import React, { useState } from "react";
 
-const index = () => {
+const PromptBuilder = () => {
+  const [instancePrompt, setInstancePrompt] = useState("");
+  const [classPrompt, setClassPrompt] = useState("");
+  const user = useUser();
+
+  console.log(instancePrompt,classPrompt)
+
   return (
     <>
       <Head>
@@ -9,17 +16,41 @@ const index = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className=" absolute inset-x-0 top-20 flex flex-col justify-center items-center">
-          <div className="rounded-md bg-black bg-opacity-20 px-8 py-6 text-sm font-medium hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-            <div className="flex flex-row">
-              <input className="promptInput" placeholder="Instance Prompt" />
-              <input className="promptInput" placeholder="Class Prompt" />
+        {user && (
+          <div className=" absolute inset-x-0 top-20 flex flex-col justify-center items-center space-y-5">
+            <div className="rounded-md bg-black bg-opacity-20 px-8 py-6 text-sm font-medium hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+              <div className="flex flex-row">
+                <input
+                  className="promptInput"
+                  placeholder="Instance Prompt"
+                  value={instancePrompt}
+                  onChange={(event) => setInstancePrompt(event.target.value)}
+                />
+                <input
+                  className="promptInput"
+                  placeholder="Class Prompt"
+                  value={classPrompt}
+                  onChange={(event) => setClassPrompt(event.target.value)}
+                />
+                <button className="submitPromptButton">Upload Render</button>
+                <button className="submitPromptButton">Submit Prompt</button>
+              </div>
             </div>
-          </div>
+            <div className="promptListWrapper text-slate-500">
+            <ul>
+                <li>prompt 1</li>
+                <li>prompt 1</li>
+                <li>prompt 1</li>
+                <li>prompt 1</li>
+            </ul>
+
         </div>
+          </div>
+        )}
+       
       </main>
     </>
   );
 };
 
-export default index;
+export default PromptBuilder;
