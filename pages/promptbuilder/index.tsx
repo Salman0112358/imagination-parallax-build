efdbb33fde99e7e6ea7c3c5292df0071210d681f
Salman0112358 <Salman0112358@outlook.com>
2022-11-03 14:18:18 +0000

    Added a refresh function to rerender the prompts list upon a new prompt submission

--- pages/promptbuilder/index.tsx
+++ pages/promptbuilder/index.tsx
@@ -1,6 +1,7 @@
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import { supabase } from "../../utils/supabaseClient";
 import { IoMdCopy } from "react-icons/io";
+import { useRouter } from "next/router";
 
 import Head from "next/head";
 import React, { useEffect, useState } from "react";
@@ -18,12 +19,17 @@ const PromptBuilder = ({ data }: any) => {
     setPromptArray(data);
   }, [data]);
 
+  const router = useRouter();
   const supabaseClient = useSupabaseClient();
   const user = useUser();
 
   const handleClear = () => {
     setInstancePrompt("");
     setClassPrompt("");
+  };
+
+  const refreshPromptList = () => {
+    router.replace(router.asPath);
   };
 
   const handlePromptSubmission = async () => {
@@ -46,68 +52,81 @@ const PromptBuilder = ({ data }: any) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        
-          <div className=" absolute inset-x-0 top-20 flex flex-col justify-center items-center space-y-5">
-            <div className="outlineBox">
-              <div className="flex flex-row">
-                <textarea
-                  className="promptTextArea w-[100vh]"
-                  name="prompt"
-                  placeholder="Enter your prompt ideas here"
-                  rows={6}
-                  value={promptIdea}
-                  onChange={(event) => setPromptIdea(event.target.value)}
-                />
-                <button className="submitPromptButton">Upload Render</button>
-                <button
-                  className="submitPromptButton"
-                  onClick={handlePromptSubmission}
-                >
-                  Submit Prompt
-                </button>
-              </div>
-            </div>
-
-            <div className="outlineBox">
-              <div className="flex flex-row">
-                <input
-                  className="promptInput"
-                  placeholder="Instance Prompt"
-                  value={instancePrompt}
-                  onChange={(event) => setInstancePrompt(event.target.value)}
-                />
-                <input
-                  className="promptInput"
-                  placeholder="Class Prompt"
-                  value={classPrompt}
-                  onChange={(event) => setClassPrompt(event.target.value)}
-                />
-                <button className="submitPromptButton" onClick={handleClear}>
-                  Clear
-                </button>
-              </div>
-            </div>
-
-            <div className="promptListWrapper text-slate-500">
-              <ul className="space-y-5">
-                {promptArray.map((prompt: IPrompt) => (
-                  <div
-                    className="outlineBox  flex flex-row"
-                    key={prompt.id}
-                  >
-                    <div className=" w-4/5">{replaceInstanceAndClass(prompt.prompt , instancePrompt, classPrompt)}</div>
-                    <div className=" relative w-1/5">
-                      <IoMdCopy
-                        fill="white"
-                        className="absolute text-slate-500  inset-y-0 right-0 cursor-pointer"
-                        onClick={() => handleCopy(prompt.prompt) }
-                      />
-                    </div>
-                  </div>
-                ))}
-              </ul>
+        <div className=" absolute inset-x-0 top-20 flex flex-col justify-center items-center space-y-5">
+          <div className="outlineBox">
+            <div className="flex flex-row">
+              <textarea
+                className="promptTextArea w-[100vh]"
+                name="prompt"
+                placeholder="Enter your prompt ideas here"
+                rows={6}
+                value={promptIdea}
+                onChange={(event) => setPromptIdea(event.target.value)}
+              />
+              <button className="submitPromptButton">Upload Render</button>
+              <button
+                className="submitPromptButton"
+                onClick={async () => {
+                  await handlePromptSubmission();
+                  refreshPromptList();
+                }}
+              >
+                Submit Prompt
+              </button>
             </div>
           </div>
+
+          <div className="outlineBox">
+            <div className="flex flex-row">
+              <input
+                className="promptInput"
+                placeholder="Instance Prompt"
+                value={instancePrompt}
+                onChange={(event) => setInstancePrompt(event.target.value)}
+              />
+              <input
+                className="promptInput"
+                placeholder="Class Prompt"
+                value={classPrompt}
+                onChange={(event) => setClassPrompt(event.target.value)}
+              />
+              <button className="submitPromptButton" onClick={handleClear}>
+                Clear
+              </button>
+            </div>
+          </div>
+
+          <div className="promptListWrapper text-slate-500">
+            <ul className="space-y-5">
+              {promptArray.map((prompt: IPrompt) => (
+                <div className="outlineBox  flex flex-row" key={prompt.id}>
+                  <div className=" w-4/5">
+                    {replaceInstanceAndClass(
+                      prompt.prompt,
+                      instancePrompt,
+                      classPrompt
+                    )}
+                  </div>
+                  <div className=" relative w-1/5">
+                    <IoMdCopy
+                      fill="white"
+                      className="absolute text-slate-500  inset-y-0 right-0 cursor-pointer"
+                      onClick={() =>
+                        handleCopy(
+                          replaceInstanceAndClass(
+                            prompt.prompt,
+                            instancePrompt,
+                            classPrompt
+                          )
+                        )
+                      }
+                    />
+                  </div>
+                </div>
+              ))}
+            </ul>
+          </div>
+        </div>
       </main>
     </>
   );
