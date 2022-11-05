72d48d15a3928490119d89ce4b601df3f8d02527
Salman0112358 <Salman0112358@outlook.com>
2022-11-05 21:40:34 +0000

    util function handles cases for only having an instance, class and neither inputs filled out and returns a default prompt string template instead in the final case

--- utils/replaceInstanceAndClass.ts
+++ utils/replaceInstanceAndClass.ts
@@ -3,10 +3,19 @@ const replaceInstanceAndClass = (
   userInstance: string,
   userClass: string
 ): string => {
+  if (userInstance && !userClass)
+    return prompt.replace("{INSTANCE_PROMPT}", userInstance);
 
-  
+  if (userClass && !userInstance)
+    return prompt.replace("{CLASS_PROMPT}", userClass);
 
-  return prompt.replace("{CLASS_PROMPT}", userClass).replace("{INSTANCE_PROMPT}", userInstance)
+
+  if (userClass && userInstance) return prompt
+  .replace("{CLASS_PROMPT}", userClass)
+  .replace("{INSTANCE_PROMPT}", userInstance);
+
+  return prompt;
+
 };
 
 export default replaceInstanceAndClass;
