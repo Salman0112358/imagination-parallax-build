7e30f20d5501884840673ba668faf42ad8512481
Salman0112358 <Salman0112358@outlook.com>
2022-11-10 20:31:28 +0000

    now added a user profile to the log in and can now add a avatar image by using an image url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 // 1. import `NextUIProvider` component
 import "../styles/globals.css";
 import { useState } from "react";
-import { ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import type { AppProps } from "next/app";
 import NavbarHeader from "../components/NavbarHeader/NavbarHeader";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session;
+}>) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
 
   return (
@@ -20,18 +24,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <NavbarHeader />
         <Component {...pageProps} />
       </SessionContextProvider>
-      {/* <ToastContainer
-        position="top-center"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      /> */}
     </>
   );
 }
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,29 +1,37 @@
-import React from 'react'
-import type {NextPage} from 'next'
-import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+import React from "react";
+import type { NextPage } from "next";
+import {
+  useSupabaseClient,
+  useSession,
+} from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
-import {Auth, ThemeSupa} from "@supabase/auth-ui-react"
+import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
+import Account from "../../components/Account/Account";
 
 const Login: NextPage = () => {
-
+  
   const supabaseClient = useSupabaseClient();
-  const user = useUser();
+  const session = useSession();
   const router = useRouter();
 
-  if (user) {
-    router.push("/")
-}
+  // if (user) {
+  //   router.push("/login/account");
+  // }
 
   return (
     <div className=" flex justify-center items-center h-screen w-screen">
-      <div className=" w-1/4">
-        <Auth
-          supabaseClient={supabaseClient}
-          appearance={{ theme: ThemeSupa }}
-          theme="dark"
-          providers={["discord"]}
-        />
-      </div>
+      {!session ? (
+        <div className=" w-1/4">
+          <Auth
+            supabaseClient={supabaseClient}
+            appearance={{ theme: ThemeSupa }}
+            theme="dark"
+            providers={["discord"]}
+          />
+        </div>
+      ) : (
+        <Account session={session}/>
+      )}
     </div>
   );
 };
