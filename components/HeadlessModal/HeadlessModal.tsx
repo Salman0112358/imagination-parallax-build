4919bdb5ee37e44a1f593acbe5a8d43b8c1d2c0c
Salman0112358 <Salman0112358@outlook.com>
2022-11-11 02:49:32 +0000

    z index change for modal

--- components/HeadlessModal/HeadlessModal.tsx
+++ components/HeadlessModal/HeadlessModal.tsx
@@ -1,15 +1,13 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
 import { singleImage } from "../../typescript";
-import { toast} from 'react-toastify';
+import { toast } from "react-toastify";
 
 import Image from "next/image";
 import handleCopy from "../../utils/handleCopy";
 
 export default function HeadlessModal({ image }: singleImage) {
   let [isOpen, setIsOpen] = useState(false);
-
-
 
   const notify = () => toast("Prompt Copied To Clipboard");
 
@@ -32,69 +30,74 @@ export default function HeadlessModal({ image }: singleImage) {
           Show More
         </button>
       </div>
+      <div className="  z-50 ">
+        <Transition appear show={isOpen} as={Fragment}>
+          <Dialog as="div" className="relative z-10" onClose={closeModal}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-black bg-opacity-25" />
+            </Transition.Child>
 
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel
-                  className={` ${
-                    image.width > image.height ? "w-[90vh]" : "w-[50vh]"
-                  } transform overflow-hidden rounded-2xl bg-black text-slate-100 p-6 text-left align-middle shadow-xl transition-all`}
+            <div className="fixed inset-0 overflow-y-auto">
+              <div className="flex min-h-full items-center justify-center p-4 text-center">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
                 >
-                  <div className="flex flex-col">
-                    <img className="rounded-md" src={image.src} alt="AI Art" />
-                  </div>
-                  <div className="mt-2">
-                    <p className="text-sm">{image.prompt}</p>
-                    <div className=" mt-3 flex-col text-sm font-light">
-                      <p>Guidance Scale : {image.guidance}</p>
-                      <p>Image Seed : {image.seed}</p>
-                      <p>
-                        Dimensions : {image.width} X {image.height}
-                      </p>
+                  <Dialog.Panel
+                    className={` ${
+                      image.width > image.height ? "w-[90vh]" : "w-[50vh]"
+                    } transform overflow-hidden rounded-2xl bg-black text-slate-100 p-6 text-left align-middle shadow-xl transition-all`}
+                  >
+                    <div className="flex flex-col">
+                      <img
+                        className="rounded-md"
+                        src={image.src}
+                        alt="AI Art"
+                      />
                     </div>
-                  </div>
+                    <div className="mt-2">
+                      <p className="text-sm">{image.prompt}</p>
+                      <div className=" mt-3 flex-col text-sm font-light">
+                        <p>Guidance Scale : {image.guidance}</p>
+                        <p>Image Seed : {image.seed}</p>
+                        <p>
+                          Dimensions : {image.width} X {image.height}
+                        </p>
+                      </div>
+                    </div>
 
-                  <div className="mt-4">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-violet-900 px-4 py-2 text-sm font-medium text-slate-100 hover:bg-violet-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={() => {
-                        handleCopy(image.prompt);
-                        notify()
-                      }}
-                    >
-                      Copy Prompt
-                    </button>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
+                    <div className="mt-4">
+                      <button
+                        type="button"
+                        className="inline-flex justify-center rounded-md border border-transparent bg-violet-900 px-4 py-2 text-sm font-medium text-slate-100 hover:bg-violet-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                        onClick={() => {
+                          handleCopy(image.prompt);
+                          notify();
+                        }}
+                      >
+                        Copy Prompt
+                      </button>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
             </div>
-          </div>
-        </Dialog>
-      </Transition>
+          </Dialog>
+        </Transition>
+      </div>
     </>
   );
 }
