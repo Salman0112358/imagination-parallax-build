927ad8f89eb096725bda37388cabfd5e2c074eb8
Salman0112358 <Salman0112358@outlook.com>
2022-11-21 16:17:17 +0000

    got rid of css that wasn't being applied to the prompt card

--- components/PromptListCard/PromptListCard.tsx
+++ components/PromptListCard/PromptListCard.tsx
@@ -18,7 +18,7 @@ const PromptListCard = ({
     <>
       <div className="relative opacity-60">
         <button
-          className="absolute rounded-3xl right-0 w-1/4 hidden group-hover:block font-light m-1 hover:scale-90"
+          className="absolute rounded-3xl right-0 w-1/4 m-1 hidden group-hover:block font-light"
           onClick={() =>
             handleCopy(
               replaceInstanceAndClass(
@@ -31,40 +31,23 @@ const PromptListCard = ({
         >
           Copy
         </button>
-        <button className="absolute rounded-3xl left-0 w-1/4 hidden group-hover:block font-light m-1 hover:scale-90">
+        <button className="absolute rounded-3xl left-0 w-1/4 m-1 hidden group-hover:block font-light">
           Kudos
         </button>
       </div>
-      <div className=" absolute bottom-0 w-full overflow-hidden  opacity-70">
-        {/* <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2 text-violet-300"> <p className="text-white font-light text-sm">
-            {replaceInstanceAndClass(
-              prompt.prompt,
-              instancePrompt,
-              classPrompt
-            )}
-          </p></div>
-         
-        </div> */}
-        <div className="px-4 py-4 font-light">
-          <span className="cursor-pointer inline-block bg-black rounded-full px-3 py-1 text-sm  text-white mr-2 mb-2">
+      <div className="absolute bottom-0 w-full opacity-70 text-[0px]">
+        <div className="px-4 py-1 font-light">
+          <span className="inline-block bg-black rounded-full px-3 py-1 text-sm  text-white">
             Posted by : {prompt.username}
           </span>
-          <span className="cursor-pointer inline-block bg-black rounded-full px-3 py-1 text-sm  text-white mr-2 mb-2">
+          <span className="inline-block bg-black rounded-full px-3 py-1 text-sm  text-white">
             Dimensions : {prompt.natural_width} X {prompt.natural_height}
           </span>
           {prompt.guidance_scale && (
-            <span className="cursor-pointer inline-block bg-black rounded-full px-3 py-1 text-sm  text-white mr-2 mb-2">
+            <span className="inline-block bg-black rounded-full px-3 py-1 text-sm  text-white">
               Guidance : {prompt.guidance_scale}
             </span>
           )}
-
-          {/* <span className="cursor-pointer inline-block bg-black rounded-full px-3 py-1 text-sm  text-white mr-2 mb-2">
-            Sampling Method : {prompt.sampling_method}
-          </span>
-          <span className="cursor-pointer inline-block bg-black rounded-full px-3 py-1 text-sm  text-white mr-2 mb-2">
-            Seed : {prompt.seed}
-          </span> */}
         </div>
       </div>
     </>
