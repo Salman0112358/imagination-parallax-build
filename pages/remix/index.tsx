2f427b8ee5f3bc3e6aee2c8a39eb271ac4ac1748
Salman0112358 <Salman0112358@outlook.com>
2022-11-22 23:11:00 +0000

    cleaning up the remix page route

--- pages/remix/index.tsx
+++ pages/remix/index.tsx
@@ -1,16 +1,10 @@
 import { supabase } from "../../utils/supabaseClient";
-
 import Head from "next/head";
-import React, { useEffect, useState } from "react";
-import { useUser } from "@supabase/auth-helpers-react";
-import PromptSubmission from "../../components/PromptSubmission/PromptSubmission";
+import React, {useState } from "react";
 import { IPrompt } from "../../typescript";
-import router from "next/router";
-import { GenericModal } from "../../components/GenericModal/GenericModal";
-import extractUserRemixSubmissions from "../../utils/extractUserRemixSubmissions";
 import PromptListCard from "../../components/PromptListCard/PromptListCard";
-import PromptInstanceAndClassInput from "../../components/PromptInstanceAndClassInput/PromptInstanceAndClassInput";
 import ImageGrid from "../../components/ImageGrid/ImageGrid";
+import RemixToolBar from "../../components/RemixToolBar/RemixToolBar";
 
 interface IPromptBuilder {
   data: IPrompt[];
@@ -19,82 +13,30 @@ interface IPromptBuilder {
 const PromptBuilder = ({ data }: IPromptBuilder) => {
   const [instancePrompt, setInstancePrompt] = useState("");
   const [classPrompt, setClassPrompt] = useState("");
-  const [sortedData, setSortedData] = useState<IPrompt[]>([]);
-  const [userSubmissions, setUserSubmissions] = useState<IPrompt[]>([]);
-
-  const user = useUser();
-
-  useEffect(() => {
-    handleSortOrder("new");
-    user && setUserSubmissions(extractUserRemixSubmissions(data, user?.id));
-  }, [user]);
-
-  const handleSortOrder = (sort: string) => {
-    if (sort === "random") {
-      setSortedData(data.sort(() => Math.random() - 0.5));
-    } else {
-      setSortedData(data);
-    }
-    router.replace(router.asPath, undefined, { scroll: false });
-  };
-
+  const [sortedData, setSortedData] = useState<IPrompt[]>(data);
+  
   return (
     <>
       <Head>
         <title>Imagine A Prompt</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className="mt-[8vh]">
-        <div className=" fixed left-0 top-[30%] m-2 z-[50] max-[720px]:hidden outlineBox  space-y-1 flex flex-col justify-center ">
-          <button
-            title="Sort By Random"
-            onClick={() => handleSortOrder("random")}
-          >
-            🎲
-          </button>
-          <button title="Sort By Latest" onClick={() => handleSortOrder("new")}>
-            🔥
-          </button>
-          <button
-            title="Show Only Your Submissions"
-            onClick={() => setSortedData(userSubmissions)}
-          >
-            👤
-          </button>
-
-          <GenericModal modalText="💡">
-            <PromptSubmission />
-          </GenericModal>
-          <GenericModal modalText="🖊️">
-            <div className="flex flex-row">
-              <PromptInstanceAndClassInput
-                instancePrompt={instancePrompt}
-                classPrompt={classPrompt}
-                setInstancePrompt={setInstancePrompt}
-                setClassPrompt={setClassPrompt}
-              />
-            </div>
-          </GenericModal>
-          <button
-            title="Clear The Instance And Class"
-            onClick={() => {
-              setClassPrompt("");
-              setInstancePrompt("");
-            }}
-          >
-            🧻
-          </button>
-        </div>
         <div className=" px-[1vw] ">
+          <RemixToolBar
+            sortedData={sortedData}
+            setSortedData={setSortedData}
+            instancePrompt={instancePrompt}
+            classPrompt={classPrompt}
+            setInstancePrompt={setInstancePrompt}
+            setClassPrompt={setClassPrompt}
+            data={data} />
           <ImageGrid>
             {sortedData.map((image: IPrompt) => (
               <div
-                className={` group card ${
-                  image.natural_height / image.natural_width > 1 && "card-tall"
-                } ${
-                  image.natural_height / image.natural_width < 1 && "card-wide"
-                }   `}
+                className={` group card ${image.natural_height / image.natural_width > 1 && "card-tall"
+                  } ${image.natural_height / image.natural_width < 1 && "card-wide"
+                  }   `}
                 key={image.id}
                 style={{ backgroundImage: `url('${image.render_image}')` }}
               >
