6d17b6aa9794764df41f094ecd56d1db381e3cbc
Salman0112358 <Salman0112358@outlook.com>
2022-11-23 18:14:26 +0000

    added local storage persistence for instance and class names

--- components/PromptInstanceAndClassInput/PromptInstanceAndClassInput.tsx
+++ components/PromptInstanceAndClassInput/PromptInstanceAndClassInput.tsx
@@ -1,40 +1,44 @@
 import React from "react";
+import { IPromptInstanceAndClassInput, IUserInstanceAndClass } from "../../typescript";
 
-interface IPromptInstanceAndClassInput {
-  instancePrompt: string;
-  classPrompt: string;
-  setInstancePrompt: React.Dispatch<React.SetStateAction<string>>;
-  setClassPrompt: React.Dispatch<React.SetStateAction<string>>;
-}
+const PromptInstanceAndClassInput = ({ userInstanceAndClass, setUserInstanceAndClass }: IPromptInstanceAndClassInput) => {
 
-const PromptInstanceAndClassInput = ({
-  instancePrompt,
-  setInstancePrompt,
-  classPrompt,
-  setClassPrompt,
-}: IPromptInstanceAndClassInput) => {
-  const handleClear = () => {
-    setInstancePrompt("");
-    setClassPrompt("");
+  const handleInstanceAndClass = (
+    userInstanceAndClass: IUserInstanceAndClass,
+    setUserInstanceAndClass: React.Dispatch<React.SetStateAction<IUserInstanceAndClass>>,
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { name, value } = e.target;
+    setUserInstanceAndClass({ ...userInstanceAndClass, [name]: value });
   };
-
   return (
     <div className="outlineBox w-[50vw] p-1">
       <div className="flex items-center space-x-5">
         <input
+          name="instancePrompt"
           className="promptInput h-[5vh]"
           placeholder="Instance Prompt "
-          value={instancePrompt}
-          onChange={(event) => setInstancePrompt(event.target.value)}
+          onChange={(e) => handleInstanceAndClass(userInstanceAndClass, setUserInstanceAndClass, e)}
         />
         <input
+          name="classPrompt"
           className="promptInput h-[5vh]"
           placeholder="Class Prompt"
-          value={classPrompt}
-          onChange={(event) => setClassPrompt(event.target.value)}
+          onChange={(e) => handleInstanceAndClass(userInstanceAndClass, setUserInstanceAndClass, e)}
         />
-        <button className="" onClick={handleClear}>
-          Clear
+        <button className="" onClick={() => {
+          setUserInstanceAndClass({ instancePrompt: "", classPrompt: "" });
+          localStorage.setItem("instance", "{INSTANCE_PROMPT}");
+          localStorage.setItem("class", "{CLASS_PROMPT}");
+
+        }}>
+          Reset
+        </button>
+        <button className="" onClick={() => {
+          localStorage.setItem("instance", userInstanceAndClass.instancePrompt)
+          localStorage.setItem("class", userInstanceAndClass.classPrompt)
+        }}>
+          Apply
         </button>
       </div>
     </div>
